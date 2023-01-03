08e9779c909cb1ae10fa4e6c2898835d006bf3af
azizdev <aziz16110904@gmail.com>
2023-01-03 18:38:35 +0500

    add create method

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home
+from .views import home, create
 urlpatterns = [
-    path('', home)
+    path('', home),
+    path('create/',create, name="create"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from .models import Person
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    people = Person.objects.all()
+    return render(request, 'index.html', {"people": people})
+
+def create(request):
+    vname = request.POST.get('name')
+    Person.objects.create(name=vname)
+    people = Person.objects.all()
+    return render(request, 'index.html', {"people": people})
+    
